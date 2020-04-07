a523bcaf0377a5f8f1c0bda0ad26d238ef7ebcef
NanZ-hash <afnanxmt@gmail.com>
2020-04-07 14:43:18 +0300

    creating the story model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_110542) do
+ActiveRecord::Schema.define(version: 2020_04_07_113943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_04_07_110542) do
     t.index ["user_id"], name: "index_jokes_on_user_id"
   end
 
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
@@ -45,4 +54,5 @@ ActiveRecord::Schema.define(version: 2020_04_07_110542) do
 
   add_foreign_key "examples", "users"
   add_foreign_key "jokes", "users"
+  add_foreign_key "stories", "users"
 end
